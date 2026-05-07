3b7887a52ad1fcf89b2c3dadc4f805ea82400669
Habibur Rahman <habib@isoldien.com>
2026-05-08 00:35:57 +0100

    feat: get steamID as a integer from csv and cleaned up some code in main.py

--- main.py
+++ main.py
@@ -2,14 +2,16 @@ import menu
 import steam_user
 from steam_user import *
 import steam_id
+from steam_id import *
 
 # Load menu
 menu.start()
 
 # search user
-# put results into csv
 steam_user.userSearch()
-#steam_user.dictToCSV()
-
-# get steam id
+# put results into csv
+steam_user.dictToCSV()
+# get SteamID
 steam_id.getSteamID()
+# print SteamID 
+print(steam_id.steamID)
--- steam_id.py
+++ steam_id.py
@@ -1,10 +1,14 @@
 import pandas as pd
 
 def getSteamID() :
+    global steamID
+
     df = pd.read_csv('csv/search_results.csv')
     df = df.filter(['steamid'])
-    print(df)
-    print(df.columns)
+
+    steamID = int(df['steamid'][0])
+
+    return steamID
 
 
 
