6654cfdd544241095fc1ca571264150254c3c936
Habibur Rahman <habib@isoldien.com>
2026-05-07 22:17:46 +0100

    feat: add dictionary to csv export

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-import menu 
+import menu
+
 import steam_user
 from steam_user import *
 
 # Load menu
+menu.start()
+
+# search user
+# put results into csv
+steam_user.userSearch()
+steam_user.dictToCSV()
--- steam_user.py
+++ steam_user.py
@@ -1,5 +1,7 @@
 import os
 from steam_web_api import Steam
+import csv
+import pandas as pd
 
 KEY = os.environ.get("STEAM_WEB_API") # get key from system env, dependant on OS 
 steam = Steam(KEY)
@@ -7,6 +9,24 @@ steam = Steam(KEY)
 # search for a user 
 def userSearch() :
     global name, search
-    name = input('What is your Steam Username?' + '\n')
+    name = str(input('What is your Steam Username?' + '\n'))
     search = steam.users.search_user(name)
     return name, search
+
+# convert dictioniary to CSV
+def dictToCSV() :
+    # @TODO add exception if directory exists 
+    # @TODO add exception if file already exists
+
+    # create directory and filename
+    os.mkdir("csv")
+    csv_filename = "csv/search_results"
+
+    # convert dict to csv
+    with open(csv_filename, mode='w', newline='') as file:
+        writer = csv.writer(file)
+        writer.writerow(search.keys())
+        writer.writerow(search.values())
+    
+
+
